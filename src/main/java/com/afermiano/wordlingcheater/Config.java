b629f5cd43bfae2020a6b417de60c8ce85c8ce69
Antonio Fermiano <afermianophone@gmail.com>
2022-10-18 08:48:39 -0300

    Adding GPL headers

--- src/main/java/com/afermiano/wordlingcheater/Config.java
+++ src/main/java/com/afermiano/wordlingcheater/Config.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2022 Antonio Fermiano
+ *
+ * This file is part of Wordling Cheater.
+ *
+ * Wordling Cheater is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Wordling Cheater is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Wordling Cheater. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package com.afermiano.wordlingcheater;
 
 import com.afermiano.wordlingcheater.guesser.scorestrategy.MostCommonLettersInWord;
